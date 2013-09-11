3cb8b890b7c10fc8932a2cd7ca34a08739903c09
franszo <franszo.design@gmail.com>
2013-09-11 17:30:16 +0100

    icons changed to icon fonts
    Signed-off-by: franszo <franszo.design@gmail.com>

--- js/lte-ie7.js
+++ js/lte-ie7.js
@@ -6,30 +6,14 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'icons\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-tick' : '&#x61;',
-			'icon-twitter' : '&#x37;',
-			'icon-stars' : '&#x39;',
-			'icon-ribbon-bear' : '&#x71;',
-			'icon-pinterest' : '&#x34;',
-			'icon-phone' : '&#x35;',
-			'icon-person' : '&#x36;',
-			'icon-open-quote' : '&#x77;',
-			'icon-info' : '&#x65;',
-			'icon-google-plus' : '&#x72;',
-			'icon-facebook' : '&#x74;',
-			'icon-expand' : '&#x79;',
-			'icon-camera' : '&#x75;',
-			'icon-arrow-up' : '&#x69;',
-			'icon-arrow-right' : '&#x6f;',
-			'icon-arrow-left' : '&#x31;',
-			'icon-arrow-down' : '&#x32;',
-			'icon-alert' : '&#x33;',
-			'icon-untitled' : '&#x70;',
-			'icon-shopping-bag' : '&#x73;',
-			'icon-ce-logo' : '&#x64;'
+			'icon-kishandchips' : '&#x31;',
+			'icon-facebook' : '&#x32;',
+			'icon-twitter' : '&#x33;',
+			'icon-chevron-left' : '&#x34;',
+			'icon-chevron-right' : '&#x35;'
 		},
 		els = document.getElementsByTagName('*'),
-		i, attr, html, c, el;
+		i, attr, c, el;
 	for (i = 0; ; i += 1) {
 		el = els[i];
 		if(!el) {
